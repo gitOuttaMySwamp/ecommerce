f1ac51fe676700c46967dafba4db0b49f24ae4f1
Garron <garronvgj11@gmail.com>
2020-04-02 17:37:47 -0500

    added 10 pieces

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -6,4 +6,19 @@ class Piece < ApplicationRecord
   # has_many :pieceOrder
 
   validates :name, :sizeId, :detailId, presence: true
+
+  paginates_per 5
+  max_paginates_per 100
+
+  def self.search(search, size, page)
+    where_clause = ''
+    if !search.to_s.empty?
+      where('name LIKE "%' + search + '%"')
+        .where('sizeId = ' + size)
+        .page(page)
+    else
+      all.where('sizeId = ' + size)
+         .page(page)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :details
   resources :sizes
   resources :pieces
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'pieces#index'
+  get '/home/about'
 end
