cbe396eb7292a466f111a81b9681f713ae314467
Garron <garronvgj11@gmail.com>
2020-04-01 20:02:59 -0500

    updated detail, piece, and size models

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Detail < ApplicationRecord
+  has_many :piece
+
+  validates :price, presence: true
 end
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class Piece < ApplicationRecord
+  belongs_to :size, foreign_key: :sizeId
+  belongs_to :detail, foreign_key: :detailId
+  # has_many :pieceOrder
+
+  validates :name, :sizeId, :detailId, presence: true
 end
