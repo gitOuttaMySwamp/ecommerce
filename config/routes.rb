8ae83979cc6770849e55c09471d0a041c4318cca
Garron <garronvgj11@gmail.com>
2020-04-07 22:38:47 -0500

    console logging

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+  end
   resources :details
   resources :sizes
   resources :pieces
