d07bbd6c982c21964d7ad133c186964093b6af35
Garron <garronvgj11@gmail.com>
2020-04-08 19:12:42 -0500

    added sign up buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'pieces#index'
+  get 'pages/home'
+  devise_for :users
+
   resources :details
   resources :sizes
   resources :pieces
 
-  root to: 'pieces#index'
   get '/home/about'
   post 'pieces/add_to_cart/:id', to: 'pieces#add_to_cart', as: 'add_to_cart'
   delete 'pieces/remove_from_cart/:id', to: 'pieces#remove_from_cart', as: 'remove_from_cart'
